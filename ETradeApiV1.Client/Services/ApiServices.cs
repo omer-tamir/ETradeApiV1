39761979eaf0813314ed504e2f4981c26d792199
Omer Tamir <omer_mina@hotmail.com>
2019-01-20 21:24:24 +1000

    add .Framework console app

--- ETradeApiV1.Client/Services/ApiServices.cs
+++ ETradeApiV1.Client/Services/ApiServices.cs
@@ -1,28 +1,57 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Web;
 using ETradeApiV1.Client.Models;
 using Microsoft.Extensions.Configuration;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace ETradeApiV1.Client.Services
 {
     public class ApiServices
     {
-        private readonly OAuthSetting _oAuthSetting;
+
         public ApiServices()
         {
-             Configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory()) // Directory where the json files are located
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
+            Configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory()) // Directory where the json files are located
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .Build();
 
-            _oAuthSetting= Configuration.GetSection("OAuthSetting").Get<OAuthSetting>();
+            OAuthSetting = Configuration.GetSection("OAuthSetting").Get<OAuthSetting>();
         }
 
         private IConfiguration Configuration { get; }
+        private OAuthSetting OAuthSetting { get; set; }
 
-
-        public OAuthSetting GetSetting()
+        public string GetAuthorizeUrl()
         {
-            return _oAuthSetting;
+            var baseUrl = new Uri(OAuthSetting.BaseUrl);
+            var client = new RestClient(baseUrl)
+            {
+                Authenticator = OAuth1Authenticator.ForRequestToken(OAuthSetting.ConsumerKey, OAuthSetting.ConsumerSecret, "oob")
+            };
+
+            var request = new RestRequest("/oauth/request_token");
+            var response = client.Execute(request);
+
+            var qs = HttpUtility.ParseQueryString(response.Content);
+            var oauthToken = qs["oauth_token"];
+            var oauthTokenSecret = qs["oauth_token_secret"];
+            var applicationName = qs["application_name"];
+
+            var baseSslUrl = new Uri(OAuthSetting.AuthorizeUrl);
+            var sslClient = new RestClient(baseSslUrl);
+
+            request = new RestRequest("authorize");
+            request.AddParameter("key", OAuthSetting.ConsumerSecret);
+            request.AddParameter("token", oauthToken);
+
+
+            var url = sslClient.BuildUri(request).ToString();
+
+            return url;
         }
+
     }
 }
---
+++ ETradeApiV1.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETradeApiV1.Client.Services;
+
+namespace ETradeApiV1.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiService = new ApiServices();
+            var authorizeUrl = apiService.GetAuthorizeUrl();
+
+            System.Console.WriteLine($"{authorizeUrl}");
+
+
+            Process.Start(authorizeUrl);
+
+            string verificationKey;
+
+            do
+            {
+                System.Console.Write("Enter verification key: ");
+
+                verificationKey = System.Console.ReadLine();
+            }
+            while (verificationKey == null);
+        }
+    }
+}
