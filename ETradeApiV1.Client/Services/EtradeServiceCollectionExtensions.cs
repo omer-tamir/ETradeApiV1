cd49e453733a91d45f9e287fa2f4be413d2d77d3
Omer Tamir <omer_mina@hotmail.com>
2019-04-07 19:30:01 +1000

    AddEtradeService

---
+++ ETradeApiV1.Client/Services/EtradeServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETradeApiV1.Client.Services
+{
+    public static class EtradeServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEtradeService(this IServiceCollection collection,Action<EtApiServiceOptions> setupAction, ServiceLifetime contextLifetime = ServiceLifetime.Singleton)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            if (setupAction == null) throw new ArgumentNullException(nameof(setupAction));
+
+            collection.Configure(setupAction);
+            switch (contextLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    return collection.AddSingleton<EtApiService, EtApiService>();
+                case ServiceLifetime.Transient:
+                    return collection.AddTransient<EtApiService, EtApiService>();
+                default:
+                    return collection.AddScoped<EtApiService, EtApiService>();
+            }
+        }
+    }
+}
