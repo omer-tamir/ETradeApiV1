d9e163ac28b7d8d4fed9937054069e007a5fb46f
Omer Tamir <omer_mina@hotmail.com>
2019-07-07 09:05:33 +1000

    Service Registration

--- ETradeApiV1.Client/EtradeServiceRegistration.cs
+++ ETradeApiV1.Client/EtradeServiceRegistration.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using ETradeApiV1.Client.Interfaces;
+using ETradeApiV1.Client.Services;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ETradeApiV1.Client.Services
+namespace ETradeApiV1.Client
 {
-    public static class EtradeServiceCollectionExtensions
+    /// <summary>
+    /// IServiceCollection extensions for registering Etrade Api
+    /// </summary>
+    public static class EtradeServiceRegistration
     {
+        /// <summary>
+        /// Register Etrade Api.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="setupAction"></param>
+        /// <param name="contextLifetime"></param>
+        /// <returns></returns>
         public static IServiceCollection AddEtradeService(this IServiceCollection collection,Action<EtApiServiceOptions> setupAction, ServiceLifetime contextLifetime = ServiceLifetime.Singleton)
         {
             if (collection == null) throw new ArgumentNullException(nameof(collection));
@@ -22,7 +33,11 @@ namespace ETradeApiV1.Client.Services
                     return collection.AddScoped<IEtApiService, EtApiService>();
             }
         }
-
+        /// <summary>
+        /// Register Etrade Api.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
         public static IServiceCollection AddEtradeService(this IServiceCollection collection)
         {
             if (collection == null) throw new ArgumentNullException(nameof(collection));
